7da560685970e811bbcd098df0998e76cc129ca8
Kaan Elman <k.elman@hotmail.de>
2026-01-04 19:27:27 +0100

    feat: add login tests and i18n

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,26 @@
 	let { children } = $props();
 </script>
 
+<script context="module">
+  	import { waitLocale } from 'svelte-i18n'
+
+	import { getLocaleFromNavigator, init, register, } from 'svelte-i18n';
+
+	register('en', () => import('../translations/en.json'));
+	register('de', () => import('../translations/de.json'));
+
+init({
+  fallbackLocale: 'en',
+  initialLocale: getLocaleFromNavigator()
+});
+  
+
+  	export async function preload() {
+    // awaits for the loading of the 'en-US' and 'en' dictionaries
+    return waitLocale()
+  }
+</script>
+
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
