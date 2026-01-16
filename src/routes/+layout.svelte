3b7da4325edfeb8cc63439b8dff038324f81d943
Bnyro <bnyro@tutanota.com>
2026-01-16 14:54:13 +0100

    feat: add header with navigation bar/drawer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,25 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-	let { children } = $props();
-	let title="OKR Project"
+import favicon from "$lib/assets/favicon.svg";
+import { MenuIcon, MoreHorizontalIcon, X } from "@lucide/svelte";
+import "../app.css";
+
+let { children } = $props();
+let title = "OKR Project";
 </script>
 
 <script context="module">
-  	import { waitLocale } from 'svelte-i18n'
-
+	import { waitLocale } from 'svelte-i18n'
 	import { getLocaleFromNavigator, init, register, } from 'svelte-i18n';
 
 	register('en', () => import('../translations/en.json'));
 	register('de', () => import('../translations/de.json'));
 
-init({
-  fallbackLocale: 'en',
-  initialLocale: getLocaleFromNavigator()
-});
-  
+  init({
+    fallbackLocale: 'en',
+    initialLocale: getLocaleFromNavigator()
+  });
 
-  	export async function preload() {
+	export async function preload() {
     // awaits for the loading of the 'en-US' and 'en' dictionaries
     return waitLocale()
   }
@@ -29,13 +30,46 @@ init({
 	<title>{title}</title>
 </svelte:head>
 
-<nav id="navbar">
-	<a href="/">Home</a>
-	<a href="/login">Login</a>
-	<a href="/user">User</a>
-	<a href="/projects">Projects</a>
-	<a href="/objectives">Objectives</a>
-	<a href="/key_results">Key results</a>
-</nav>
+<div class="navbar bg-base-100 shadow-sm">
+  <div class="drawer w-auto">
+    <!-- uses the CSS checkbox hack, see https://daisyui.com/components/drawer/ -->
+    <input id="navigation-drawer" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content">
+      <!-- Actual content of the button used for toggling the drawer -->
+      <label for="navigation-drawer" class="btn btn-square btn-ghost">
+        <MenuIcon />
+      </label>
+    </div>
+    <div class="drawer-side">
+      <!-- close drawer on click outside (i.e. click on this drawer-overlay label) -->
+      <label for="navigation-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+
+      <div class="bg-base-200 min-h-full w-80 p-4 flex flex-col">
+        <!-- button to manually close the navigation drawer -->
+        <label for="navigation-drawer" aria-label="close sidebar" class="btn btn-ghost w-min self-end"><X /></label>
+        
+        <ul class="menu w-full">
+        	<li><a href="/">Home</a></li>
+        	<li><a href="/login">Login</a></li>
+        	<li><a href="/user">User</a></li>
+        	<li><a href="/projects">Projects</a></li>
+        	<li><a href="/objectives">Objectives</a></li>
+        	<li><a href="/key_results">Key results</a></li>
+        </ul>
+       </div>
+    </div>
+  </div>
+  <div class="flex-1">
+    <a class="btn btn-ghost text-xl" href="/">OKR-ng</a>
+  </div>
+  <!-- button with more options, e.g. logout -->
+  <div class="dropdown dropdown-end">
+    <div tabindex="0" role="button" class="btn btn-square btn-ghost m-1"><MoreHorizontalIcon /></div>
+    <ul tabindex="-1" class="dropdown-content menu bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
+      <li class="btn btn-ghost">Logout <!-- TODO: logout actually --></li>
+      <li class="btn btn-ghost">Something else</li>
+    </ul>
+  </div>
+</div>
 
 {@render children()}
