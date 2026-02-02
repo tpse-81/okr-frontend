26320cde6ba6fde32930d3d3be25d0157500b9f5
Bnyro <bnyro@tutanota.com>
2026-02-02 14:13:57 +0100

    fix: validate username + email input

--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -13,8 +13,9 @@ async function screateUser() {
 
 <h1>Create User</h1>
 <form id="user-submit" onsubmit={screateUser} class="flex gap-3 p-3 m-auto">
-    <input type="text" bind:value={name} placeholder="Name" class="input w-full">
-    <input type="text" bind:value={passwort} placeholder="passwort" class="input w-full">
-    <input type="text" bind:value={email} placeholder="Email" class="input w-full">
+		<!-- username pattern allows everything but '@' characters -->
+    <input type="text" pattern="^((?!@).)*$" bind:value={name} placeholder="Name" class="input w-full" required>
+    <input type="text" bind:value={passwort} placeholder="passwort" class="input w-full" required>
+    <input type="email" bind:value={email} placeholder="Email" class="input w-full" required>
     <input type="submit" value="Create" class="btn btn-primary">
 </form>
