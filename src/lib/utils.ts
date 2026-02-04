4fbd1bf7aef2644b1383cc8aec0c49dc1bd584f0
Javes64 <yavuzsenguel64@gmail.com>
2026-02-04 13:52:27 +0100

    fix: remove unused code

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -42,10 +42,3 @@ export function formatDate(date: Date): string {
 		day: "numeric",
 	});
 }
-
-export function getCookie(name: string) {
-	if (typeof document === "undefined") {return undefined;}
-	const value = `; ${document.cookie}`;
-	const parts = value.split(`; ${name}=`);
-	if (parts.length === 2) return parts.pop()?.split(';')[0];
-}
