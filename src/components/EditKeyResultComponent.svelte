bbadff1891f490238e5017087d0c8910af6d67b0
Javes64 <yavuzsenguel64@gmail.com>
2026-02-05 16:47:12 +0100

    fix: pull reuest

--- src/components/EditKeyResultComponent.svelte
+++ src/components/EditKeyResultComponent.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 import { updateKeyResult } from "$lib/api";
 import type { KeyResult } from "$lib/types";
+import {isBetween} from "$lib/utils";
+import { fade } from "svelte/transition";
 
 let {
 	show,
@@ -11,7 +13,8 @@ let {
 	keyResult: KeyResult;
 	ondismiss: () => void;
 } = $props();
-
+let errorMessage: string = $state("");
+let errorTimeout: ReturnType<typeof setTimeout> | null = null;
 // svelte-ignore non_reactive_update
 let modal: HTMLDialogElement;
 // svelte-ignore state_referenced_locally
@@ -20,16 +23,36 @@ let description: string = $state(keyResult.description);
 let startValue: number = $state(keyResult.start_value);
 // svelte-ignore state_referenced_locally
 let endValue: number = $state(keyResult.end_value);
+// svelte-ignore state_referenced_locally
+let currentValue: number = $state(keyResult.current_value);
 
 $effect(() => {
 	if (show) modal.showModal();
 	else modal.close();
 });
 
+function showError(message: string) {
+    errorMessage = message;
+
+    if (errorTimeout) {
+        clearTimeout(errorTimeout);
+    }
+
+    errorTimeout = setTimeout(() => {
+        errorMessage = "";
+        errorTimeout = null;
+    }, 1500);
+}
+
 async function onUpdatekeyResult() {
+    if (!isBetween(currentValue, startValue, endValue)) {
+        showError("Current value is out of bounds");
+        return;
+    }
 	keyResult.description = description;
 	keyResult.start_value = startValue;
 	keyResult.end_value = endValue;
+    keyResult.current_value = currentValue;
 
 	await updateKeyResult(keyResult);
 
@@ -43,15 +66,25 @@ async function onUpdatekeyResult() {
     <form class="flex flex-col gap-3 mt-3">
 	    <input type="text" bind:value={description} placeholder="description" class="input w-full">
 	    <input type="number" bind:value={startValue} placeholder="start value" class="input w-full">
+        <input type="number" bind:value={currentValue} placeholder="current value" class="input w-full">
 	    <input type="number" bind:value={endValue} placeholder="end value" class="input w-full">
     </form>
-    <div class="modal-action">
-      <form method="dialog" class="flex gap-3 w-full justify-end">
-        <button class="btn" onclick={ondismiss}>Close</button>
-        <!-- if there is a button in form, it will close the modal -->
-        <button class="btn btn-primary" onclick={onUpdatekeyResult}>Confirm</button>
-      </form>
-    </div>
+      <div class="modal-action">
+          <div class="flex gap-3 w-full justify-end">
+              <button type="button" class="btn" onclick={ondismiss}>Close</button>
+              <button type="button" class="btn btn-primary" onclick={onUpdatekeyResult}>Confirm</button>
+          </div>
+      </div>
+
+      {#if errorMessage}
+          <div
+                  in:fade={{ duration: 120 }}
+                  out:fade={{ duration: 240 }}
+                  class="alert alert-error absolute bottom-2 left-2 right-2 z-10 p-2 text-sm"
+          >
+              {errorMessage}
+          </div>
+      {/if}
   </div>
 </dialog>
 
