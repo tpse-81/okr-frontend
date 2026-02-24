36c6c7bbd7d73ff1ca2390df553f2b0b3a604373
Bnyro <bnyro@tutanota.com>
2026-02-24 22:43:37 +0100

    feat: show current task state as badge

--- src/components/Task.svelte
+++ src/components/Task.svelte
@@ -7,7 +7,7 @@
   ```
 -->
 <script lang="ts">
-import { Edit, Trash } from "@lucide/svelte";
+import { Edit, Info, Trash } from "@lucide/svelte";
 import { _ } from "svelte-i18n";
 import { deleteTask } from "$lib/api";
 import type { Task } from "$lib/types";
@@ -32,6 +32,11 @@ async function onDeleteTask() {
 </script>
 
 <li class="card card-border relative">
+  <div class="absolute left-2 top-2 badge badge-info">
+    <Info size="16" />
+    <span>{$_(`tasks.${task.task_state}`)}</span>
+  </div>
+
   <div class="absolute right-2 top-2 flex gap-2">
     <button type="button" class="btn btn-square" onclick={() => (showEditDialog = true)}>
       <Edit size="16" />
@@ -40,17 +45,14 @@ async function onDeleteTask() {
       <Trash size="16" />
     </button>
   </div>
-<div class="card-body">
-  <h3 class="card-title">{title}</h3>
 
-  {#if details?.trim()}
-    <p>{details}</p>
-  {/if}
+  <div class="card-body pt-12">
+    <h3 class="card-title">{title}</h3>
 
-  <div class="text-sm opacity-70">
-    {$_("tasks.state")}: {$_(`tasks.${task.task_state}`)}
+    {#if details?.trim()}
+      <p>{details}</p>
+    {/if}
   </div>
-</div>
 </li>
 
 <EditTaskComponent show={showEditDialog} task={task} ondismiss={() => showEditDialog = false} />
